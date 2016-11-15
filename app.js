95025123a68d5f912f8d9e7301a7321fcf0d0ee2
saidyuce <saidyuce94@gmail.com>
2016-11-15 18:14:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var io = require('/usr/local/lib/node_modules/socket.io').listen(65080);
+var io = require('/usr/local/lib/node_modules/socket.io').listen(65079);
 
 io.sockets.on('connection', function (socket) {
-  console.log('Server listening on port: 65080');
+  console.log('Server listening on port: 65079');
   var address = socket.handshake.address;
   console.log('New connection from ' + address.address + ':' + address.port);
 
 io.sockets.emit('this', { will: 'be received by everyone'});
 
-socket.on('private message', function (msg) {
+socket.on('email', function (msg) {
 console.log('New Chat Message ', msg);
-io.sockets.emit('txt',msg);
+io.sockets.emit('callback',msg);
 });
 
 socket.on('disconnect', function () {
